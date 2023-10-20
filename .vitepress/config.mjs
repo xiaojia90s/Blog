266174a17af3ccc42ef3dde928130ab8592fd992
wsj001 <981461889@qq.com>
2023-10-20 23:49:34 +0800

    临时提交。

--- .vitepress/config.mjs
+++ .vitepress/config.mjs
@@ -2,6 +2,7 @@ import { defineConfig } from "vitepress";
 
 // https://vitepress.dev/reference/site-config
 export default defineConfig({
+  base: "./",
   title: "菜的抠jio的二狗子",
   description: "二狗子的个人博客",
   themeConfig: {
